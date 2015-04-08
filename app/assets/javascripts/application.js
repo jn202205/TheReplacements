b7b2beb4bc27bc4a6167447cd9d252bb310cd6cc
Jonathan Newman <jn202205@gmail.com>
2015-04-08 15:56:30 -0700

    remove dup require jquery, add files to directories so heroku doesnt fail

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require underscore
 //= require backbone
 //= require app
