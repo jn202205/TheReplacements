bb3317a65ef37ddd8587eac41b09312d9bd98f04
Jonathan Newman <jn202205@gmail.com>
2015-04-10 10:27:17 -0700

    use snake case filename, require get_or_fetch in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require backbone
 //= require app
 //= require composite_view
+//= require get_or_fetch
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
