7b2b975d370c7b5f761916c890aad173b4907c2e
Jonathan Newman <jn202205@gmail.com>
2015-04-10 11:23:16 -0700

    remove unnecessary bb views

--- app/assets/javascripts/views/layouts/header.js
+++
@@ -1,9 +0,0 @@
-App.Views.Header = Backbone.View.extend({
-  template: JST['layouts/header'],
-  className: 'header',
-
-  render: function() {
-    this.$el.html(this.template());
-    return this;
-  }
-});
