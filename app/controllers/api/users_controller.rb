fccc40af7a77a0e21b237f850186b08e7989d765
Jonathan Newman <jn202205@gmail.com>
2015-04-10 23:55:58 -0700

    allow passing limit for number of players

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,17 @@
 module Api
   class UsersController < ApplicationController
     def index
-      @users = User.all
+      limit = params[:limit].to_i
+      if limit > 0
+        @users = User.where(zipcode: current_user.zipcode).sample(limit)
+      else
+        @users = User.all
+        # @users = User.where(zipcode: current_user.zipcode)
+      end
     end
 
     def show
-      @user = User.find(params[:id]) 
+      @user = User.find(params[:id])
     end
   end
 end
