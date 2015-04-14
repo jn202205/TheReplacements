8e39168ad5c74b3a866218bffc5763e7cca569c9
Jonathan Newman <jn202205@gmail.com>
2015-04-14 14:35:55 -0700

    add experience as a user attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414045056) do
+ActiveRecord::Schema.define(version: 20150414213226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20150414045056) do
     t.datetime "updated_at",                                 null: false
     t.string   "img_url"
     t.string   "playing_area"
+    t.text     "experience"
   end
 
   add_index "users", ["captain_id"], name: "index_users_on_captain_id", using: :btree
