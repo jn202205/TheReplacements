0da949371b4c7e46443118e92e670cf9647962d0
Jonathan Newman <jn202205@gmail.com>
2015-04-15 16:47:15 -0700

    use images as sport selectors

--- app/assets/javascripts/views/sports/sport_form_sidebar.js
+++ app/assets/javascripts/views/sports/sport_form_sidebar.js
@@ -2,13 +2,31 @@ App.Views.SportsFormSidebar = Backbone.CompositeView.extend({
   template: JST['sports/form_sidebar'],
   className: 'signup-form',
 
+  events: {
+    'submit': 'submitForm',
+    'click label.sport': 'selectSport'
+  },
+
+  selectSport: function(event) {
+    var selectedSport = $(event.currentTarget);
+    if ($(event.target).is(':checked')) {
+      selectedSport.children('img').addClass('selected');
+    } else {
+      selectedSport.children('img').removeClass('selected');
+    }
+  },
+
+  preSelectSports: function() {
+    this.$('input[type=checkbox]').each(function(idx, sport) {
+      if ($(sport).is(':checked')) {
+        $(sport).siblings('img').addClass('selected');
+      }
+    });
+  },
+
   initialize: function() {
     this.listenTo(this.model, 'sync', this.render);
     this.listenTo(this.collection, 'sync', this.render);
-  },
-
-  events: {
-    'submit': 'submitForm'
   },
 
   render: function() {
@@ -16,7 +34,7 @@ App.Views.SportsFormSidebar = Backbone.CompositeView.extend({
       sports: this.collection
     });
     this.$el.html(content);
-
+    this.preSelectSports();
     return this;
   },
 
@@ -25,7 +43,11 @@ App.Views.SportsFormSidebar = Backbone.CompositeView.extend({
     var player = new App.Models.Player({
       id: App.currUser.id,
     });
-    player.save($(event.target).serializeJSON(), { wait: true });
-    Backbone.history.navigate('', { trigger: true });
+    player.save($(event.target).serializeJSON(), {
+      wait: true
+    });
+    Backbone.history.navigate('', {
+      trigger: true
+    });
   }
 });
