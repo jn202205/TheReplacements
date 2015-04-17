713eaeb5f2efaa98a09ab6c752958f910d093c54
Jonathan Newman <jn202205@gmail.com>
2015-04-17 14:34:41 -0700

    button to player profile

--- app/assets/javascripts/views/players/player_card.js
+++ app/assets/javascripts/views/players/player_card.js
@@ -2,12 +2,24 @@ App.Views.PlayerCard = Backbone.View.extend({
   template: JST['players/player_card'],
   className: "player-card",
 
+  events: {
+    'click a.profile-btn': 'gotoProfile'
+  },
+
+  gotoProfile: function(event) {
+    event.preventDefault();
+    var player_id = $(event.target).data('id');
+    Backbone.history.navigate('player/' + player_id, { trigger: true });
+  },
+
   initialize: function() {
     this.listenTo(this.model, 'sync', this.render);
   },
 
   render: function() {
-    var content = this.template({ player: this.model });
+    var content = this.template({
+      player: this.model
+    });
     this.$el.html(content);
 
     return this;
